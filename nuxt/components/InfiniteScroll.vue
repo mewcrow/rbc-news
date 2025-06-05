1d44a6f51d072a44447e55cdeefdd337bcc45d7d
Kaydalov Vitaly <kaidalov@avtoalfa.com>
2025-06-05 16:50:58 +0300

    News infinite scroll

---
+++ nuxt/components/InfiniteScroll.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex justify-center">
+    <span v-if="loading" class="loading loading-bars loading-md" />
+  </div>
+</template>
+
+<script setup lang="ts">
+  const apiClient = useSanctumClient()
+  const loading = ref(false);
+  let page = 1;
+  const emit = defineEmits<{
+    pageFetched: [page: unknown]
+  }>()
+  const props = defineProps<{ apiUrl: string }>()
+
+  if (import.meta.client) {
+    window.addEventListener('scroll', () => {
+      if (loading.value) return;
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        page++;
+        loadNextPage(page);
+      }
+    });
+  }
+
+  async function loadNextPage(page: number) {
+    loading.value = true;
+    apiClient(`${props.apiUrl}?page=${page}`)
+      .then((res: unknown) => {
+        emit('pageFetched', res as { data: unknown[] })
+        loading.value = false
+      })
+      .finally(() => loading.value = false)
+  }
+</script>
