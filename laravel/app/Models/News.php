a2fc3a59a3562cbd00be043e0e98d7d58baf92ba
Kaydalov Vitaly <kaidalov@avtoalfa.com>
2025-09-15 14:55:44 +0300

    bakc1

--- laravel/app/Models/News.php
+++ laravel/app/Models/News.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -10,6 +11,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class News extends Model
 {
+    use CrudTrait;
     use Sluggable, HasFactory, SoftDeletes;
 
     protected $fillable = [
